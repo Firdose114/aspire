5466e5c297319c805fb3821f720cd1c10a3789b4
Firdose Siddique <Firdose Siddique@Home>
2022-01-31 07:14:04 +0400

    jan31...prorams

---
+++ mit-aspire/src/febaspire/Hello.java
@@ -0,0 +1,7 @@
+package febaspire;
+
+public class Hello {
+public static void main(String[] args) {
+	System.out.println("Hello---welcome to java.");
+}
+}
---
+++ mit-aspire/src/febaspire/process.java
@@ -0,0 +1,10 @@
+package febaspire;
+
+public class process {
+public static void main(String[] args) {
+	int a=12;
+	int b=12;
+	int c=a+b;
+	System.out.println("sum of"+a+"and"+b+"is"+c);
+}
+}
